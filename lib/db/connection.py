29cbe2370c7985986a8634a44dee3afac195b658
Mash <ruelamacharia2007@gmail.com>
2025-06-02 18:47:27 +0300

    Update connection.py

--- lib/db/connection.py
+++ lib/db/connection.py
@@ -1,6 +1,8 @@
+
 import sqlite3
 
+connection = sqlite3.connect('db/articles.db')
+connection.row_factory = sqlite3.Row
+
 def get_connection():
-    conn = sqlite3.connect("your_database.db")
-    conn.row_factory = sqlite3.Row  # enables row["column_name"] access
-    return conn
+    return connection
