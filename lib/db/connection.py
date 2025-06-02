01588c639783727141173d25f03503b427eab9a9
Mash <ruelamacharia2007@gmail.com>
2025-06-02 17:45:44 +0300

    Update connection.py

--- lib/db/connection.py
+++ lib/db/connection.py
@@ -1,9 +1,6 @@
-import os
 import sqlite3
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # lib/db
-DB_PATH = os.path.join(BASE_DIR, "articles.db")        # lib/db/articles.db
-
 def get_connection():
-    conn = sqlite3.connect(DB_PATH)
+    conn = sqlite3.connect("your_database.db")
+    conn.row_factory = sqlite3.Row  # enables row["column_name"] access
     return conn
